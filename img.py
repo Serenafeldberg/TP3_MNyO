5b4c65242bd361d5f3905f2eddea17ee734268df
Serenafeldberg <sfeldberg@udesa.edu.ar>
2023-05-28 19:14:17 -0300

    1 completo ?

--- img.py
+++ img.py
@@ -27,27 +27,69 @@ print("flattened_data shape", flattened_data.shape)
 # Perform singular value decomposition
 U, s, Vt = np.linalg.svd(flattened_data)
 
-print(U.shape)
-print(s.shape)
-print(Vt.shape)
+first_10_dimensions = U[:, :10]
+last_10_dimensions = U[:, -10:]
 
-# Visualize the first 10 dimensions (eigenvectors)
-fig, axs = plt.subplots(2, 8, figsize=(10, 4))
-fig.suptitle('16 images', fontsize=16)
+# plt.imshow(first_10_dimensions, cmap='gray')
+# plt.title('First 10 Dimensions (Eigenvectors)')
+# plt.show()
 
-for i, ax in enumerate(axs.flatten()):
-    ax.imshow(data[i], cmap='gray')
-    ax.axis('off')
+# plt.imshow(last_10_dimensions, cmap='gray')
+# plt.title('Last 10 Dimensions (Eigenvectors)')
+# plt.show()
+
+
+
+# fig, axs = plt.subplots(2, 8, figsize=(10, 4))
+# fig.suptitle('16 images', fontsize=16)
+
+# for i, ax in enumerate(axs.flatten()):
+#     ax.imshow(data[i], cmap='gray')
+#     ax.axis('off')
 
 #plt.show()
 
-# Visualize the first 10 dimensions (eigenvectors)
-fig, axs = plt.subplots(2, 5, figsize=(10, 4))
-fig.suptitle('First 10 Dimensions (Eigenvectors)')
 
-for i, ax in enumerate(axs.flatten()):
-    print(U.shape)
-    ax.imshow(U, cmap='gray')
-    ax.axis('off')
+# fig, axs = plt.subplots(2, 10, figsize=(20, 4))
+# fig.tight_layout()
 
+# # Visualizar las primeras 10 dimensiones como imágenes
+# for i in range(10):
+#     axs[0, i].imshow(first_10_dimensions[:, i].reshape(data.shape[0], -1), cmap='gray')
+#     axs[0, i].axis('off')
+
+# # Visualizar las últimas 10 dimensiones como imágenes
+# for i in range(10):
+#     axs[1, i].imshow(last_10_dimensions[:, i].reshape(data.shape[0], -1), cmap='gray')
+#     axs[1, i].axis('off')
+
+# Mostrar la figura
+# plt.show()
+
+image_to_compress = data[0]
+
+U, s, Vt = np.linalg.svd(image_to_compress)
+
+Sd = np.diag(s)
+k  = 120
+comprimida = U[:,:k] @ Sd[0:k,:k] @ Vt[:k,:]
+plt.imshow(comprimida, cmap='gray')
 plt.show()
+
+# Definir el porcentaje máximo de error permitido (en este caso, 5%)
+max_error_percentage = 0.05
+
+# Inicializar el número mínimo de dimensiones requeridas
+min_dimensions = 1
+
+# Calcular el error inicial
+reconstructed_image = U[:, :min_dimensions] @ np.diag(s[:min_dimensions]) @ Vt[:min_dimensions, :]
+error = np.linalg.norm(image_to_compress - reconstructed_image, ord='fro') / np.linalg.norm(image_to_compress, ord='fro')
+
+# Incrementar el número de dimensiones hasta que se cumpla el error máximo
+while error > max_error_percentage:
+    min_dimensions += 1
+    reconstructed_image = U[:, :min_dimensions] @ np.diag(s[:min_dimensions]) @ Vt[:min_dimensions, :]
+    error = np.linalg.norm(image_to_compress - reconstructed_image, ord='fro') / np.linalg.norm(image_to_compress, ord='fro')
+
+print("Número mínimo de dimensiones requeridas:", min_dimensions)
